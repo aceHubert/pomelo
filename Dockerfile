07498caddade4ae79578e838c9dce873fc7a71a2
hubert <hubert@lejian.com>
2024-02-26 18:41:32 +0800

    chore: deploy with docker

--- Dockerfile
+++ Dockerfile
@@ -1,78 +1,80 @@
-# DOCKER_BUILDKIT=1 docker build -t pomelo:0.0.2 --target deploy --cache-from pomelo:0.0.1 --build-arg BUILD_IGNORE=true --build-arg BUILDKIT_INLINE_CACHE=1 .
-# docker run -it -d -p 3001:3001 -p 3002:3002 -p 3003:3003 -p 3011:3011 -p 3012:3012 -e ENV_FILE=.env.local --name pomelo-dev --rm pomelo:0.0.2
+# DOCKER_BUILDKIT=1 docker build -t hubert007/pomelo:0.0.1 -t hubert007/pomelo:latest --target deploy --cache-from hubert007/pomelo:latest --build-arg BUILD_IGNORE=true --build-arg BUILDKIT_INLINE_CACHE=1 .
+# docker run -it -d -p 3001:3001 -p 3002:3002 -p 3003:3003 -p 3011:3011 -p 3012:3012 -e ENV_FILE=.env.local --name pomelo-server --rm hubert007/pomelo:0.0.1
 
-# 添加ARG参数，可在docker build时通过 --build-arg BUILD_OPTION="xxx" 传入代码编译命令
+# 添加ARG参数，可在docker build时通过 --build-arg BUILD_OPTION="xxx" --build-arg  BUILD_PLATFORM="xxx"传入代码编译命令
+# 容器 nodejs 版本
 ARG NODE_VERSION="16.14.0-alpine"
-# true: 不编译代码，直接使用缓存；false: 编译代码
-ARG BUILD_IGNORE="false"
-# 编译选项 yarn build:${BUILD_OPTION}
-ARG BUILD_OPTION="vue"
-# 运行环境
-ARG RUNTIME_ENV="production"
-# 运行指定应用
-ARG RUNTIME_APPS="identity-server,identity-api,infrastructure-api,client-admin,client-web"
+# 编译平台架构
+ARG BUILD_PLATFORM="linux/amd64"
 
-FROM node:${NODE_VERSION} AS builder
+FROM --platform=${BUILD_PLATFORM} node:${NODE_VERSION} AS builder
 ADD . /app
 WORKDIR /app
 # 添加ARG参数，可在docker build时通过 --build-arg BUILD_OPTION="xxx" 传入代码编译命令
-ARG BUILD_IGNORE
-ARG BUILD_OPTION
+# true: 不编译代码，直接使用缓存；false: 编译代码
+ARG BUILD_IGNORE="false"
+# 编译选项 yarn ${BUILD_OPTION}
+ARG BUILD_OPTION="build"
 
 # 编译 Nodejs 文件
 RUN if [ "$BUILD_IGNORE" = "true" ]; then \
         set -x ; echo "Build ignored!"; \
     else \
-        set -ex && pwd && ls && yarn install --mode=skip-build && yarn build:${BUILD_OPTION} && ls; \
+        set -ex && pwd && ls && yarn install --mode=skip-build && yarn ${BUILD_OPTION} && ls; \
     fi
 
-FROM node:${NODE_VERSION} AS runtime
+FROM --platform=${BUILD_PLATFORM} node:${NODE_VERSION} AS runtime
 COPY --from=builder /app/packages /app/packages/
 COPY --from=builder /app/servers /app/servers/
-COPY --from=builder /app/clients/vue-admin/dist /app/clients/admin/
-COPY --from=builder /app/clients/vue-web/dist /app/clients/web/
+COPY --from=builder /app/clients/vue-admin/dist /app/clients/client-admin/
+COPY --from=builder /app/clients/vue-web/dist /app/clients/client-web/
 COPY --from=builder /app/ecosystem.config.js /app/
+COPY --from=builder /app/nginx.conf /app/
 COPY --from=builder /app/package*.json /app/
 COPY --from=builder /app/.yarn /app/.yarn/
 COPY --from=builder /app/.yarnrc.yml /app/
 COPY --from=builder /app/yarn.lock /app/
+
+# 删除不必要的文件(安装依赖前，减小 node_modules 文件大小)
+RUN rm -rf /app/packages/antdv-layout-pro \
+        /app/packages/pomelo-shared-client \
+        /app/packages/pomelo-theme \
+        /app/packages/*/src \
+        /app/servers/*/src
+
+WORKDIR /app
+# 安装 Nodejs 运行时文件
+# https://v3.yarnpkg.com/cli/workspaces/focus
+RUN yarn workspaces focus --production --all
+
+# 删除不必要的文件(安装依赖后，依赖文件)
 RUN find /app/packages/*/babel.config.js \
         /app/packages/*/builder.config.ts \
         /app/packages/*/Gruntfile.js \
         /app/packages/*/tsconfig.json \
         /app/packages/*/tsconfig.*.json \
+        /app/servers/*/package.json \
         /app/servers/*/tsconfig.json \
         /app/servers/*/tsconfig.*.json \
-        -type f | xargs rm -rf && \
-    rm -rf /app/packages/antdv-layout-pro \
-        /app/packages/pomelo-shared-client \
-        /app/packages/pomelo-theme \
-        /app/packages/*/src \
-        /app/servers/*/src \
-        /app/servers/identity-server/public/style/less
+        -type f | xargs rm -rf
 
-ARG RUNTIME_ENV
-
-WORKDIR /app
-ENV NODE_ENV=${RUNTIME_ENV}
-# 安装 Nodejs 运行时文件
-# https://v3.yarnpkg.com/cli/workspaces/focus
-RUN yarn workspaces focus --production --all
-
-FROM node:${NODE_VERSION} AS deploy
+FROM --platform=${BUILD_PLATFORM} node:${NODE_VERSION} AS deploy
+COPY --from=runtime /app/servers /app/clients /app/ecosystem.config.js /app/nginx.conf /app/
 COPY --from=runtime /app/packages /app/packages/
-COPY --from=runtime /app/servers /app/servers/
-COPY --from=runtime /app/clients /app/clients/
 COPY --from=runtime /app/node_modules /app/node_modules/
-COPY --from=runtime /app/ecosystem.config.js /app/
-COPY --from=runtime /app/package*.json /app/
 
-ARG RUNTIME_ENV
-ARG RUNTIME_APPS
+# NODE_ENV 环境
+ARG RUNTIME_ENV="production"
+# PM2 环境, https://pm2.keymetrics.io/docs/usage/application-declaration/
+ARG SERVE_ENV="production"
+# 运行指定应用
+ARG SERVE_APPS=""
+
 
 WORKDIR /app
 ENV NODE_ENV=${RUNTIME_ENV}
-ENV RUNTIME_APPS=${RUNTIME_APPS}
+ENV SERVE_ENV=${SERVE_ENV}
+ENV SERVE_APPS=${SERVE_APPS}
 
 RUN npm install -g pm2
 # RUN set -ex && \
@@ -80,5 +82,9 @@ RUN npm install -g pm2
 #   npm config set registry https://registry.npm.taobao.org && \
 #   npm install -g pm2
 
-EXPOSE 3001-3003 3011-3012
-CMD pm2-runtime start ecosystem.config.js --only ${RUNTIME_APPS}
+EXPOSE 3001-3004 3011-3012
+CMD if [ "$SERVE_APPS" = "" ]; then \
+      pm2-runtime start ecosystem.config.js --env $SERVE_ENV; \
+    else \
+      pm2-runtime start ecosystem.config.js --only "$SERVE_APPS" --env $SERVE_ENV; \
+    fi
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,28 +1,158 @@
+// show swagger documentation
+const SWAGGER_DEBUG = false;
+// show graphql playground
+const GRAPHQL_DEBUG = false;
+// origin
+const ORIGIN = 'https://example.com';
+// cors origin
+const CORS_ORIGIN = [];
+// infrastructure database connection
+const INFRASTRUCTURE_DATABASE_CONNECTION = 'mysql://user:password@host:port/database';
+// identity database connection
+const IDENTITY_DATABASE_CONNECTION = 'mysql://user:password@host:port/database';
+// redis connection
+const REDIS_URL = 'redis://host:port/db';
+
 module.exports = {
   apps: [
     {
       name: 'identity-server',
-      cwd: 'servers/identity-server',
+      cwd: 'identity-server',
       script: 'node dist/main.js',
       autorestart: true,
+      env_production: {
+        PORT: 3001,
+        GLOBAL_PREFIX_URI: '/oauth',
+        CORS_ORIGIN: CORS_ORIGIN.join('|'),
+        INFRASTRUCTURE_DATABASE_CONNECTION,
+        IDENTITY_DATABASE_CONNECTION,
+        REDIS_URL,
+        OIDC_CLIENT_SECRET: 'client-secret',
+        OIDC_ISSUER: ORIGIN + '/oauth',
+        ADMIN_URL: ORIGIN + '/admin',
+        WEB_URL: ORIGIN,
+      },
+      env_local: {
+        ENV_FILE: '.env.local',
+        DEBUG: true,
+        PORT: 3001,
+        CORS: true,
+      },
+      env_fly: {
+        ENV_FILE: '.env.fly',
+        PORT: 3001,
+        CORS_ORIGIN: ['https://pomelo-server.fly.dev:3011', 'https://pomelo-server.fly.dev:3012'].join('|'),
+        // from fly secrets
+        // INFRASTRUCTURE_DATABASE_CONNECTION,
+        // IDENTITY_DATABASE_CONNECTION,
+        // REDIS_URL,
+        OIDC_CLIENT_SECRET: 'client-secret',
+      },
     },
     {
       name: 'identity-api',
-      cwd: 'servers/identity-api',
+      cwd: 'identity-api',
       script: 'node dist/main.js',
       autorestart: true,
+      env_production: {
+        SWAGGER_DEBUG,
+        GRAPHQL_DEBUG,
+        PORT: 3002,
+        GLOBAL_PREFIX_URI: '/identity',
+        CORS_ORIGIN: CORS_ORIGIN.join('|'),
+        IDENTITY_DATABASE_CONNECTION,
+        OIDC_CLIENT_SECRET: 'ZjZiMmM2MzMtNGY5ZS00YjdhLThmMmEtOWE0YjRlOWIwYjliLlg2YWd3OFJvRUM',
+        OIDC_ISSUER: ORIGIN + '/oauth',
+        OIDC_ORIGIN: ORIGIN + '/identity',
+      },
+      env_local: {
+        ENV_FILE: '.env.local',
+        DEBUG: true,
+        PORT: 3002,
+        CORS: true,
+      },
+      env_fly: {
+        ENV_FILE: '.env.fly',
+        SWAGGER_DEBUG: true,
+        GRAPHQL_DEBUG: true,
+        PORT: 3002,
+        CORS_ORIGIN: ['https://pomelo-server.fly.dev:3011', 'https://pomelo-server.fly.dev:3012'].join('|'),
+        // from fly secrets
+        // INFRASTRUCTURE_DATABASE_CONNECTION,
+        // IDENTITY_DATABASE_CONNECTION,
+        // REDIS_URL,
+        OIDC_CLIENT_SECRET: 'client-secret',
+      },
     },
     {
       name: 'infrastructure-api',
-      cwd: 'servers/infrastructure-api',
+      cwd: 'infrastructure-api',
       script: 'node dist/main.js',
       autorestart: true,
+      env_production: {
+        SWAGGER_DEBUG,
+        GRAPHQL_DEBUG,
+        PORT: 3003,
+        GLOBAL_PREFIX_URI: '/infrastructure',
+        CORS_ORIGIN: CORS_ORIGIN.join('|'),
+        INFRASTRUCTURE_DATABASE_CONNECTION,
+        OIDC_CLIENT_SECRET: 'client-secret',
+        OIDC_ISSUER: ORIGIN + '/oauth',
+        OIDC_ORIGIN: ORIGIN + '/infrastructure',
+      },
+      env_local: {
+        ENV_FILE: '.env.local',
+        DEBUG: true,
+        PORT: 3003,
+        CORS: true,
+      },
+      env_fly: {
+        ENV_FILE: '.env.fly',
+        SWAGGER_DEBUG: true,
+        GRAPHQL_DEBUG: true,
+        PORT: 3003,
+        CORS_ORIGIN: ['https://pomelo-server.fly.dev:3011', 'https://pomelo-server.fly.dev:3012'].join('|'),
+        // from fly secrets
+        // INFRASTRUCTURE_DATABASE_CONNECTION,
+        // IDENTITY_DATABASE_CONNECTION,
+        // REDIS_URL,
+        OIDC_CLIENT_SECRET: 'client-secret',
+      },
+    },
+    {
+      name: 'static-content',
+      cwd: 'content',
+      script: 'serve',
+      env_production: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3004,
+      },
+      env_local: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3004,
+      },
+      env_fly: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3004,
+      },
     },
     {
       name: 'client-admin',
-      cwd: 'clients/admin',
+      cwd: 'client-admin',
       script: 'serve',
-      env: {
+      env_production: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3011,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html',
+      },
+      env_local: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3011,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html',
+      },
+      env_fly: {
         PM2_SERVE_PATH: '.',
         PM2_SERVE_PORT: 3011,
         PM2_SERVE_SPA: 'true',
@@ -31,9 +161,21 @@ module.exports = {
     },
     {
       name: 'client-web',
-      cwd: 'clients/web',
+      cwd: 'client-web',
       script: 'serve',
-      env: {
+      env_production: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3012,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html',
+      },
+      env_local: {
+        PM2_SERVE_PATH: '.',
+        PM2_SERVE_PORT: 3012,
+        PM2_SERVE_SPA: 'true',
+        PM2_SERVE_HOMEPAGE: '/index.html',
+      },
+      env_fly: {
         PM2_SERVE_PATH: '.',
         PM2_SERVE_PORT: 3012,
         PM2_SERVE_SPA: 'true',
