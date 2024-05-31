72844da732f64334a1a634967971b01607625a91
hubert <hubert@lejian.com>
2024-05-31 10:11:30 +0800

    feat: ckeditor image support lazyload

--- clients/vue-admin/src/components/ckeditor/plugins/gallery.ts
+++ clients/vue-admin/src/components/ckeditor/plugins/gallery.ts
@@ -14,6 +14,41 @@ export class Gallery extends Plugin {
       Gallery: '图库',
     });
 
+    editor.model.schema.extend('imageInline', { allowAttributes: 'dataSrc' });
+    editor.model.schema.extend('imageBlock', { allowAttributes: 'dataSrc' });
+
+    editor.conversion.for('upcast').attributeToAttribute({
+      view: 'data-src',
+      model: 'dataSrc',
+    });
+
+    editor.conversion.for('downcast').add((dispatcher) => {
+      dispatcher.on('attribute:dataSrc:imageInline', (evt, data, conversionApi) => {
+        _setAttribute(evt, data, conversionApi, 'imageInline');
+      });
+      dispatcher.on('attribute:dataSrc:imageBlock', (evt, data, conversionApi) => {
+        _setAttribute(evt, data, conversionApi, 'imageBlock');
+      });
+    });
+
+    const _setAttribute = (evt, data, conversionApi, type) => {
+      if (!conversionApi.consumable.consume(data.item, evt.name)) {
+        return;
+      }
+
+      const viewWriter = conversionApi.writer;
+      const figure = conversionApi.mapper.toViewElement(data.item);
+      const img = type === 'imageBlock' ? figure.getChild(0) : figure;
+
+      if (data.attributeNewValue !== null) {
+        viewWriter.setAttribute('data-src', data.attributeNewValue, img);
+        viewWriter.setAttribute('data-lazy', true, img);
+      } else {
+        viewWriter.removeAttribute('data-src', img);
+        viewWriter.removeAttribute('data-lazy', img);
+      }
+    };
+
     const componentCreator = () => {
       const button = new ButtonView();
 
@@ -27,16 +62,21 @@ export class Gallery extends Plugin {
       button.on('execute', () => {
         const selection = this.editor.model.document.selection;
         const imageUtils = this.editor.plugins.get('ImageUtils');
-        const selectionAttributes = Object.fromEntries(selection.getAttributes());
-        open({ accept: imageTypes?.map((type) => `image/${type}`).join(',') }, (url) => {
-          const selectedElement = selection.getSelectedElement();
-          if (selectedElement && imageUtils.isImage(selectedElement)) {
-            const position = this.editor.model.createPositionAfter(selectedElement);
-            imageUtils.insertImage({ ...selectionAttributes, src: url }, position);
-          } else {
-            imageUtils.insertImage({ ...selectionAttributes, src: url });
-          }
-        });
+        open(
+          { accept: imageTypes?.map((type) => `image/${type}`).join(',') },
+          (attrs: { src: string; [key: string]: any }, imageType?: 'imageBlock' | 'imageInline' | null) => {
+            if (!attrs.src) {
+              throw new Error('src is required');
+            }
+            const selectedElement = selection.getSelectedElement();
+            if (selectedElement && imageUtils.isImage(selectedElement)) {
+              const position = this.editor.model.createPositionAfter(selectedElement);
+              imageUtils.insertImage(attrs, position, imageType);
+            } else {
+              imageUtils.insertImage(attrs, null, imageType);
+            }
+          },
+        );
       });
 
       return button;
