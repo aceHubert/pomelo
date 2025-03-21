0fdbce448dd9f1a2c96f44d9dd4db376b70d5c8c
hubert <hubert@lejian.com>
2025-03-21 13:00:44 +0800

    fix: middleware not working with global prefix

--- servers/infrastructure-bff/src/main.ts
+++ servers/infrastructure-bff/src/main.ts
@@ -62,7 +62,7 @@ bootstrap<NestExpressApplication>(AppModule, {
         ? {
             path: globalPrefix,
             exclude: [
-              { path: '', method: RequestMethod.GET },
+              // { path: '', method: RequestMethod.GET }, #FIXME: 会导致 Middleware 无法生效
               { path: 'health', method: RequestMethod.GET },
             ],
           }
--- servers/infrastructure-service/src/app.module.ts
+++ servers/infrastructure-service/src/app.module.ts
@@ -3,6 +3,8 @@ import * as fs from 'fs';
 import { APP_PIPE, APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
 import { I18nModule, I18nService, I18nValidationPipe } from 'nestjs-i18n';
 import { Log4jsModule, LOG4JS_NO_COLOUR_DEFAULT_LAYOUT } from '@ace-pomelo/nestjs-log4js';
 import { configuration } from '@ace-pomelo/shared/server';
@@ -27,6 +29,8 @@ import '@/common/extends/i18n.extend';
 
 @Module({
   imports: [
+    HttpModule,
+    TerminusModule,
     ConfigModule.forRoot({
       isGlobal: true,
       expandVariables: true,
